a03fda6e111ec3115960b11cc0c3ea77768f8f14
arslanov-ngc <developer.humble@gmail.com>
2022-12-06 22:57:16 +0500

    navbar added

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const scrollLeftBtn = document.querySelector("#scroll-left"),
+    scrollRightBtn = document.querySelector("#scroll-right"),
+    promotionCardsWrapper = document.querySelector(
+      "#promotion__cards__wrapper"
+    );
+});
