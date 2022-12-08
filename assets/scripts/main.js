0b33976a5e7fe4800c9852ddec1801f880037c85
arslanov-ngc <developer.humble@gmail.com>
2022-12-08 16:51:42 +0500

    slider added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,79 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const scrollLeftBtn = document.querySelector("#scroll-left"),
-    scrollRightBtn = document.querySelector("#scroll-right"),
-    promotionCardsWrapper = document.querySelector(
-      "#promotion__cards__wrapper"
-    );
+  const promotionsParent = document.querySelector("#promotion__cards__wrapper"),
+    promotions = promotionsParent.querySelectorAll(".card"),
+    promotionsWidth = getComputedStyle(promotions[0]).width,
+    promotionsPrevBtn = document.querySelector("#promotion__scroll__left"),
+    promotionsNextBtn = document.querySelector("#promotion__scroll__right");
+
+  slider(
+    promotionsNextBtn,
+    promotionsPrevBtn,
+    promotionsWidth,
+    promotions,
+    promotionsParent
+  );
+
+  const shopsParent = document.querySelector("#shop__cards__wrapper"),
+    shops = shopsParent.querySelectorAll(".card"),
+    shopsWidth = getComputedStyle(shops[0]).width,
+    shopsPrevBtn = document.querySelector("#shops__scroll__left"),
+    shopsNextBtn = document.querySelector("#shops__scroll__right");
+
+  slider(shopsNextBtn, shopsPrevBtn, shopsWidth, shops, shopsParent);
+
+  function slider(nextBtn, prevBtn, width, items, itemsParent) {
+    let offset = 0;
+
+    prevBtn.style.display = "none";
+    prevBtn.style.opacity = "0";
+    nextBtn.style.opacity = "1";
+
+    const slicedWidth = +width.slice(0, width.length - 2) + 19;
+
+    nextBtn.addEventListener("click", () => {
+      if (offset == slicedWidth * (items.length - 4)) {
+        offset = slicedWidth * (items.length - 4);
+
+        nextBtn.style.opacity = "0";
+        setTimeout(() => {
+          nextBtn.style.display = "none";
+        }, 300);
+      } else {
+        offset += +width.slice(0, width.length - 2) + 19;
+      }
+      itemsParent.style.transform = `translateX(${-offset}px)`;
+
+      if (offset > 0) {
+        prevBtn.style.display = "flex";
+        setTimeout(() => {
+          prevBtn.style.opacity = "1";
+        }, 300);
+      }
+    });
+
+    prevBtn.addEventListener("click", () => {
+      if (offset == 0) {
+        offset = 0;
+        prevBtn.style.opacity = "0";
+        setTimeout(() => {
+          prevBtn.style.display = "none";
+        }, 300);
+      } else {
+        offset -= +width.slice(0, width.length - 2) + 19;
+      }
+      itemsParent.style.transform = `translateX(${-offset}px)`;
+
+      if (offset == slicedWidth * (items.length - 4)) {
+        nextBtn.style.opacity = "0";
+        setTimeout(() => {
+          nextBtn.style.display = "none";
+        }, 300);
+      } else {
+        nextBtn.style.display = "flex";
+        setTimeout(() => {
+          nextBtn.style.opacity = "1";
+        }, 300);
+      }
+    });
+  }
 });
