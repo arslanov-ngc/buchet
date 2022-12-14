5529cd3cfadfe4771a6c8abe2427be29ac42da59
arslanov-ngc <developer.humble@gmail.com>
2022-12-14 20:49:16 +0500

    responsive navbar added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -102,4 +102,17 @@ window.addEventListener("DOMContentLoaded", () => {
       document.body.style.overflowY = "visible";
     }
   });
+
+  // navbar
+
+  const navbarMenuBtn = document.querySelector(".navbar__MenuBtn"),
+    navbarCloseBtn = document.querySelector(".navbar__CloseBtn"),
+    navbar = document.querySelector(".navbar__content");
+
+  navbarMenuBtn.addEventListener("click", () => {
+    navbar.classList.replace("deactive", "active");
+  });
+  navbarCloseBtn.addEventListener("click", () => {
+    navbar.classList.replace("active", "deactive");
+  });
 });
--- assets/scripts/userPanel.js
+++ assets/scripts/userPanel.js
@@ -33,4 +33,17 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  // navbar
+
+  const navbarMenuBtn = document.querySelector(".navbar__MenuBtn"),
+    navbarCloseBtn = document.querySelector(".navbar__CloseBtn"),
+    navbar = document.querySelector(".navbar__content");
+
+  navbarMenuBtn.addEventListener("click", () => {
+    navbar.classList.replace("deactive", "active");
+  });
+  navbarCloseBtn.addEventListener("click", () => {
+    navbar.classList.replace("active", "deactive");
+  });
 });
